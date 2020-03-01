45fbcf08934d753965d8f2b2ae10a4ddc39d9e61
Hayden <hayden.fuller@gmail.com>
2020-03-02 10:04:04 +1100

    little fixes

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
-const form   = document.querySelector("form");
-const loader = document.querySelector('.loader')
+const form    = document.querySelector("form");
+const loader  = document.querySelector('.loader')
+const twivSelector = document.querySelector('.twivDisplay')
+
+const API     = window.location.hostname === 'localhost' ? 'http://localhost:5000/twivs' : 'https://twiver.now.sh'
 
 // setting loading gif to not display on default
 loader.style.display = "none"
-
+ 
 // event listener for submit, adding form data into an object
 form.addEventListener("submit", event => {
   event.preventDefault();
@@ -12,12 +15,62 @@ form.addEventListener("submit", event => {
   const name     = formData.get("name");
   const content  = formData.get("content");
 
-  const twiver   = {
+  const twiv = {
     name,
     content
-
   };
+  console.log(twiv);
+
+
   form.style.display   = "none"
   loader.style.display = ""
-  console.log(twiver);
+  
+  fetch(API, {
+    method: 'POST',
+    body: JSON.stringify(twiv),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(() => {
+      form.reset();
+
+        setTimeout(() => {
+          form.style.display = '';
+        }, 15000 );
+
+      listAllTwivs();
+      loader.style.display = 'none';
+  })
 }); 
+
+// function to get all the 'twivs' created and stored in the database than append them to the screen.
+listAllTwivs = () => {
+  twivSelector.innerHTML = ''
+    fetch(API)
+      .then(res => res.json())
+      .then(twivs => {
+        console.log(twivs)
+        //reversing so it comes in order or newest to old
+        twivs.reverse()
+        // a for each loop creating elements to store the twivs in
+        twivs.forEach(twiv => {
+          const div = document.createElement('div');
+
+          const header = document.createElement('h3')
+          header.textContent = twiv.name
+          div.appendChild(header)
+
+          const contents = document.createElement('p')
+          contents.textContent = twiv.content
+          div.appendChild(contents)
+
+          const date = document.createElement('small')
+          date.textContent = new Date(twiv.created)
+          div.appendChild(date)
+
+          twivSelector.appendChild(div)
+        });
+      });
+}
